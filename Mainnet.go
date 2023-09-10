f0d347401b1333ad1a68a33eb8c0399b65c03618
rashid <gagach16rus@gmail.com>
2023-09-10 09:09:01 +0300

    add: testnet

--- Mainnet.go
+++ Mainnet.go
@@ -1,1 +1,24 @@
-package network
+package main
+
+import (
+	nt "./network"
+)
+
+const (
+	TO_UPPER = iota + 1
+	TO_LOWER
+)
+
+const (
+	ADDRESS = ":8080"
+)
+
+func main() {
+	go nt.Listen(ADDRESS, handleServer)
+}
+
+func handleServer(conn nt.Conn, pack *nt.Package) {
+	nt.Handle(TO_UPPER, conn, pack, handleToUpper)
+}
+
+func handleToUpper(pack *nt.Package)
